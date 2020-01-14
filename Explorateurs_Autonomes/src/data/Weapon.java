1b8068484977b04c87043890b589fe234296d1d1
inxomny <eurasise@gmail.com>
2020-01-14 14:04:17 +0100

    Weapon: bjmeo 14/01/20 14:04

---
+++ Explorateurs_Autonomes/src/data/Weapon.java
@@ -0,0 +1,35 @@
+package data;
+
+public class Weapon extends Item {
+
+	private String effect;
+	private float damage;
+	
+	/**constructors from superclass**/
+	public Weapon() {
+		super();
+	}
+	public Weapon(String type, int price) {
+		super(type, price);
+	}
+	public Weapon(String effect, float damage) {
+		super();
+		this.effect = effect;
+		this.damage = damage; 
+	}
+	 
+	/**Getters and setters**/
+	public String getEffect() {
+		return effect;
+	}
+	public void setEffect(String effect) {
+		this.effect = effect;
+	}
+	public float getDamage() {
+		return damage;
+	}
+	public void setDamage(float damage) {
+		this.damage = damage;
+	}
+	
+}
