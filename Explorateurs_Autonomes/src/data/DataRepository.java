a0f7d5222fb81d43399d43cf309c1d51813b3441
inxomny <eurasise@gmail.com>
2020-01-19 13:09:41 +0100

    re-update of DataRepository class

--- Explorateurs_Autonomes/src/data/DataRepository.java
+++ Explorateurs_Autonomes/src/data/DataRepository.java
@@ -1,4 +1,5 @@
 package data;
+
 /**
  * this repository class allow to create and 
  * store all the simulation object 
@@ -6,4 +7,24 @@ package data;
  */
 public class DataRepository {
 
+	/**
+	 * The unique instance of the class prepared in an eager way (object created
+	 * at beginning).
+	 */
+	private static DataRepository instance = new DataRepository();
+	
+	/**
+	 * Private constructor ensuring no access from outside of the class.
+	 */
+	private DataRepository() {}
+	
+	
+	/**
+	 * Static method allows users to get the unique instance of the class.
+	 * 
+	 * @return the unique instance of the class.
+	 */
+	public static DataRepository getInstance() {
+		return instance;
+	}
 }
